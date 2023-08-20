1f19c65f5acf4c2b00db02640682fac11a5f426d
Amey <kulkarniamey02@gmail.com>
2023-08-20 23:08:32 +0530

    slice function updated

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -1,39 +1,121 @@
+// import React from "react";
+// import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+// // Import Swiper styles
+// import "swiper/css";
+// import "./Residencies.css";
+// import { sliderSettings } from "../../utils/common";
+// import PropertyCard from "../PropertyCard/PropertyCard";
+// import useProperties from "../../hooks/useProperties";
+// import {PuffLoader} from 'react-spinners'
+
+// const Residencies = () => {
+
+//   const {data, isError, isLoading} = useProperties()
+
+//   if(isError){
+//     return(
+//       <div className='wrapper'>
+//         <span>Error while fetching data</span>
+//       </div>
+//     )
+//   }
+
+//   if(isLoading){
+//     return(
+//       <div className="wrapper flexCenter" style={{height: "60vh"}}>
+//         <PuffLoader
+//         height="80"
+//         width="80"
+//         radius={1}
+//         color="#4066ff"
+//         aria-label="puff-loading"
+//         />
+//       </div>
+//     )
+//   }
+
+
+//   return (
+//     <div id="residencies" className="r-wrapper">
+//       <div className="paddings innerWidth r-container">
+//         <div className="flexColStart r-head">
+//           <span className="orangeText">Best Choices</span>
+//           <span className="primaryText">Popular Residencies</span>
+//         </div>
+//         <Swiper {...sliderSettings}>
+//           <SlideNextButton />
+//           {/* slider */}
+//           {data.slice(0, 8).map((card, i) => (
+//             <SwiperSlide key={i}>
+//               <PropertyCard card={card}/>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Residencies;
+
+// const SlideNextButton = () => {
+//   const swiper = useSwiper();
+//   return (
+//     <div className="flexCenter r-buttons">
+//       <button onClick={() => swiper.slidePrev()} className="r-prevButton">
+//         &lt;
+//       </button>
+//       <button onClick={() => swiper.slideNext()} className="r-nextButton">
+//         &gt;
+//       </button>
+//     </div>
+//   );
+// };
+
+
+
 import React from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
-// Import Swiper styles
 import "swiper/css";
 import "./Residencies.css";
 import { sliderSettings } from "../../utils/common";
 import PropertyCard from "../PropertyCard/PropertyCard";
 import useProperties from "../../hooks/useProperties";
-import {PuffLoader} from 'react-spinners'
+import { PuffLoader } from 'react-spinners'
 
 const Residencies = () => {
 
-  const {data, isError, isLoading} = useProperties()
+  const { data, isError, isLoading } = useProperties()
 
-  if(isError){
-    return(
+  if (isError) {
+    return (
       <div className='wrapper'>
         <span>Error while fetching data</span>
       </div>
     )
   }
 
-  if(isLoading){
-    return(
-      <div className="wrapper flexCenter" style={{height: "60vh"}}>
+  if (isLoading) {
+    return (
+      <div className="wrapper flexCenter" style={{ height: "60vh" }}>
         <PuffLoader
-        height="80"
-        width="80"
-        radius={1}
-        color="#4066ff"
-        aria-label="puff-loading"
+          height="80"
+          width="80"
+          radius={1}
+          color="#4066ff"
+          aria-label="puff-loading"
         />
       </div>
     )
   }
 
+  if (!Array.isArray(data)) {
+    return (
+      <div className='wrapper'>
+        <span>Data is not an array.</span>
+      </div>
+    )
+  }
 
   return (
     <div id="residencies" className="r-wrapper">
@@ -44,10 +126,9 @@ const Residencies = () => {
         </div>
         <Swiper {...sliderSettings}>
           <SlideNextButton />
-          {/* slider */}
           {data.slice(0, 8).map((card, i) => (
             <SwiperSlide key={i}>
-              <PropertyCard card={card}/>
+              <PropertyCard card={card} />
             </SwiperSlide>
           ))}
         </Swiper>
@@ -55,8 +136,6 @@ const Residencies = () => {
     </div>
   );
 };
-
-export default Residencies;
 
 const SlideNextButton = () => {
   const swiper = useSwiper();
@@ -71,3 +150,5 @@ const SlideNextButton = () => {
     </div>
   );
 };
+
+export default Residencies;
